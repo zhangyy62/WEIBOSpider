38702c355cbd1b227ac063a198de8fce89dd06a7
sparkxxxxxx <5482855@qq.com>
2020-01-07 13:37:59 +0800

    fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WEIBOSpider
 This is project that capture sina weibo datas
+参考博文 https://www.jianshu.com/p/f65829b22b91
--- src/main.py
+++ src/main.py
@@ -9,9 +9,11 @@ import time
 from lxml import etree
 from urllib.request import urlretrieve  # 用于图片下载
 
+endTime = '2019-01-01 00:00:00'
+
 # 改成自己的user_id和cookie
-user_id = 1757693565
-cookie = {"Cookie": "WEIBOCN_FROM=1110006030; ALF=1580917952; _T_WM=23874700983; MLOGIN=1; XSRF-TOKEN=25ea6c; SCF=Ah6537r_7dpXQpHU2_gjl6RK5BgzADb8STqMYONVCAcs6bmF9U5HrvpcPUAPYxN8nZ25THthblU6nJbJYs-NhGY.; SUB=_2A25zFyouDeRhGeNH41cZ8yrFzTiIHXVQ-7ZmrDV6PUJbktAKLUunkW1NSmeY2G-sDLop_tdjkhBz-DZ3W2I7Ro9H; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WWAsGrPdehLHjqxwQ0i8K7P5JpX5K-hUgL.Fo-41h-Re0B4SoB2dJLoIXnLxKnLBoML1-qLxKqL1hnL1K2LxKBLBonL1h5LxKnLBo-LBoMLxKqLBKnLB-2LxKqL1h.LB-zLxK-L12BL1heLxK-L1hqL1-zt; SUHB=0mxUHb_naevAmc; SSOLoginState=1578326654; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D100103type%253D1%2526q%253DSimon_%25E9%2598%25BF%25E6%2596%2587%26from%3D102003%26fid%3D1005051757693565%26uicode%3D10000011"}
+user_id = your_id
+cookie = {"Cookie": ""}
 # 初始url
 url = 'http://weibo.cn/%d/profile?page=1' % user_id
 # 获取初始url页面html内容，获取user_id和cookie（在返回的response header中）
@@ -28,12 +30,13 @@ word_count = 1  # 爬取的微博和图片数
 image_count = 1
 
 print('该用户微博页数 : ', pageNum)
-pageNum = 1
 
 times = 5
 one_step = int(pageNum/times)
 f = open('%d.csv' % user_id, 'w', encoding='utf-8')
 csv_writer = csv.writer(f)
+csv_writer.writerow(['内容', '赞', '转发', '评论', '是否转发'])
+
 for step in range(times):
     if step < times - 1:
         i = int(step * one_step + 1)
@@ -50,78 +53,53 @@ for step in range(times):
             lxml = requests.get(url, cookies=cookie).content
             selector = etree.HTML(lxml)
             # 获取该页面微博list
-            # content = selector.xpath('//span[@class="ctt"]')
             content = selector.xpath('//div[@class="c" and @id]')
             # 遍历每条微博
             for each in content:
                 list1 = []
                 # 获取文本内容，加入result，记录条数
                 zhuanfa = each.xpath('//span[@class="ctt"]')
-                print(111)
-                print(each.xpath('string(.)'))
-                list1.append(each.xpath('string(.)'))
-
-                # for each2 in zhuanfa:
-                #     res1 = each2.xpath('string(.)')
-                #     print('zhuanfa', res1)
-                #     list1.append(res1)
-
-                # print(222)
-                # text = each.xpath('string(.)')
-                # text = "%d: " % (word_count) + text + "\n"
-                # print('each', len(each))
+                text = each.xpath('string(.)')
+                list1.append(text)
 
                 if len(each) > 2:
-                    list1.append(each[2].xpath('a')[
-                                 last()-3].xpath('string(.)'))
-                    list1.append(each[2].xpath('a')[
-                                 last()-2].xpath('string(.)'))
-                    list1.append(each[2].xpath('a')[
-                                 last()-1].xpath('string(.)'))
-                    # print('zhuanfa', each[2].xpath('a')[1].xpath('string(.)'), each[2].xpath(
-                    #     'a')[2].xpath('string(.)'), each[2].xpath('a')[3].xpath('string(.)'))
+                    nodes = each[2].xpath('a')
+                    length = len(nodes)
+                    list1.append(nodes[length - 4].xpath('string(.)').replace('赞[', '').replace(']', ''))
+                    list1.append(nodes[length - 3].xpath('string(.)').replace('转发[', '').replace(']', ''))
+                    list1.append(nodes[length - 2].xpath('string(.)').replace('评论[', '').replace(']', ''))
                 elif len(each) == 2:
-                    list1.append(each[1].xpath('a')[
-                                 last()-3].xpath('string(.)'))
-                    list1.append(each[1].xpath('a')[
-                                 last()-2].xpath('string(.)'))
-                    list1.append(each[1].xpath('a')[
-                                 last()-1].xpath('string(.)'))
-                    # print('zhuanfa', each[1].xpath('a')[1].xpath('string(.)'), each[1].xpath(
-                    #     'a')[2].xpath('string(.)'), each[1].xpath('a')[3].xpath('string(.)'))
-
+                    nodes = each[1].xpath('a')
+                    length = len(nodes)
+                    list1.append(nodes[length - 4].xpath('string(.)').replace('赞[', '').replace(']', ''))
+                    list1.append(nodes[length - 3].xpath('string(.)').replace('转发[', '').replace(']', ''))
+                    list1.append(nodes[length - 2].xpath('string(.)').replace('评论[', '').replace(']', ''))
                 elif len(each) == 1:
-                    list1.append(each[0].xpath('a')[
-                                 last()-3].xpath('string(.)'))
-                    list1.append(each[0].xpath('a')[
-                                 last()-2].xpath('string(.)'))
-                    list1.append(each[0].xpath('a')[
-                                 last()-1].xpath('string(.)'))
-                    # print('zhuanfa', each[0].xpath('a')[1].xpath('string(.)'), each[0].xpath(
-                    # 'a')[2].xpath('string(.)'), each[0].xpath('a')[3].xpath('string(.)'))
+                    nodes = each[0].xpath('a')
+                    length = len(nodes)
+                    list1.append(nodes[length - 4].xpath('string(.)').replace('赞[', '').replace(']', ''))
+                    list1.append(nodes[length - 3].xpath('string(.)').replace('转发[', '').replace(']', ''))
+                    list1.append(nodes[length - 2].xpath('string(.)').replace('评论[', '').replace(']', ''))
 
-                # print('text', text)
-                # csv_writer.writerow([word_count, text])
+                print(text, text.find('转发了'))
+                if text.find('转发了') > -1:
+                    list1.append('是')
+                else:
+                    list1.append('否')
+
                 csv_writer.writerow(list1)
                 list1 = []
-                # result = result + text
                 word_count += 1
             print('第%d页微博内容爬取完完成' % (page))
         except:
             print('第', page, '页发生错误')
 
-        time.sleep(0.005)  # 爬取每页间隔时间
+        time.sleep(0.01)  # 爬取每页间隔时间
     print('正在进行第', step + 1, '次停顿，防止访问次数过多')
-    time.sleep(1)
+    time.sleep(3)
 
 
 try:
-    # 打开文本存放文件，如果不存在则新建
-    # fo_txt = open(os.getcwd()+"/%d" % user_id+".txt", "w")
-    # result_path = os.getcwd() + '/%d' % user_id+".txt"
-    # print('微博内容文本存放路径为 :', result_path)
-    # fo_txt.write(result)  # 将结果写入文件
-    # print('爬取成功！\n该用户微博内容：\n\n%s\n文本存放路径为%s' % (result, result_path))
     f.close()
 
 except:
